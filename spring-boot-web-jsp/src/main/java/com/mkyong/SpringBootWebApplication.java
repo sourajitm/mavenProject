36c6c98da1776e202f14d1ea9b5a9f8038fef699
sourajit_mohanty <sourajit_mohanty@BHUKRK91200L.ad.infosys.com>
2019-08-02 19:03:21 +0530

    commit for test message

--- spring-boot-web-jsp/src/main/java/com/mkyong/SpringBootWebApplication.java
+++ spring-boot-web-jsp/src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -15,6 +15,8 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
+		System.out.println("test message");
+		
 	}
 
 }
